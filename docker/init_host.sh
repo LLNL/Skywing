263ada5d83c5874b6ef2c86997238e56c2f3dc5f
mvancleaver <mvancleaver@gmail.com>
2023-01-29 03:11:03 -0800

    update chmod

--- docker/init_host.sh
+++ docker/init_host.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 arch=$(uname -m)
-
-git submodule update --init ../
+compose_version=1.29.2
 
 sudo apt-get update && sudo apt-get install -y \
     ca-certificates \
@@ -28,7 +27,7 @@ then
         docker-compose \
         docker-compose-plugin
 
-    sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     while getopts "nvidia_driver:" arg; do
@@ -44,9 +43,10 @@ then
 elif [ "$arch" == 'aarch64' ]
 then 
     echo "aarch64 Architecture Detected"
+    echo "--> Experimental / Use with Caution"
 
-    ## Adlink roscube 
-    #sudo sed -i 's/debian/ubuntu/g' /etc/apt/sources.list.d/docker.list
+    # Adlink roscube req
+    sudo sed -i 's/debian/ubuntu/g' /etc/apt/sources.list.d/docker.list
 
     sudo apt-get update && sudo apt-get install -y \
       docker-ce:arm64 \
@@ -55,7 +55,7 @@ then
       docker-compose \
       docker-compose-plugin
 
-    sudo curl -L https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
 else
